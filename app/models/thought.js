66ebbcc7f0c89867927bb2771af2efd3d8eeb4f9
Jon Evans <jle@foraker.com>
2015-05-29 15:07:45 -0600

    List thoughts
    ember g route thoughts/index
    ember g controller thoughts/index

--- app/models/thought.js
+++ app/models/thought.js
@@ -1,5 +1,13 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  body: DS.attr('string'),
+  createdAt: DS.attr('date', {
+    defaultValue: function() {
+      return new Date();
+    }
+  }),
+  hasBody: function() {
+    return this.get('body');
+  }.property('body')
 });
